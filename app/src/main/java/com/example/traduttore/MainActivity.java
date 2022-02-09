d6a57a9d5e04a9cbe135f72b7f63e0737239fcff
francy111 <biribo.francesco@studenti-ittfedifermi.edu.it>
2022-02-09 12:58:33 +0100

    Implementato menu funzionante

--- app/src/main/java/com/example/traduttore/MainActivity.java
+++ app/src/main/java/com/example/traduttore/MainActivity.java
@@ -14,22 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().setNavigationBarColor(Color.BLACK);
-
-        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,InfoActivity.class));
-            }
-        });
-        ((Button)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,MenuActivity.class));
-            }
-        });
-        ((Button)findViewById(R.id.button3)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,TranslationActivity.class));
-            }
-        });
+        startActivity(new Intent(MainActivity.this,MenuActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/traduttore/MenuActivity.java
+++ app/src/main/java/com/example/traduttore/MenuActivity.java
@@ -2,7 +2,12 @@ package com.example.traduttore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        getWindow().setNavigationBarColor(Color.BLACK);
+
+
+        ((View)findViewById(R.id.binfo1)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this,InfoActivity.class));
+            }
+        });
+        ((TextView)findViewById(R.id.binfo2)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this,InfoActivity.class));
+            }
+        });
+
+
+        ((View)findViewById(R.id.btranslate1)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this,TranslationActivity.class));
+            }
+        });
+        ((TextView)findViewById(R.id.btranslate2)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this,TranslationActivity.class));
+            }
+        });
+        ((TextView)findViewById(R.id.btranslate3)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this,TranslationActivity.class));
+            }
+        });
     }
 }
